a955317f63bc8a1e1977418424c78fea6797fbaf
xin03.liu <xin03.liu@d-robotics.cc>
2025-05-14 19:25:26 +0800

    feat(board_id)[RDK-633]: X5_MD_V0P3 continue using 5-md-v0p2.dtb
    Signed-off-by: xin03.liu <xin03.liu@d-robotics.cc>

--- debian/boot/boot.cmd
+++ debian/boot/boot.cmd
@@ -15,6 +15,7 @@ if test "${hb_board_id}" = "0x0301"; then setenv fdtfile "x5-rdk.dtb"; fi
 if test "${hb_board_id}" = "0x0302"; then setenv fdtfile "x5-rdk-v1p0.dtb"; fi
 if test "${hb_board_id}" = "0x0501"; then setenv fdtfile "x5-md-v0p1.dtb"; fi
 if test "${hb_board_id}" = "0x0502"; then setenv fdtfile "x5-md-v0p2.dtb"; setenv uart_baudrate "921600"; fi
+if test "${hb_board_id}" = "0x0503"; then setenv fdtfile "x5-md-v0p2.dtb"; setenv uart_baudrate "921600"; fi
 
 echo fdtfile = ${fdtfile}
 
